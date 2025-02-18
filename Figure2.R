ad267121a2f7c4ad8c1d0307b54a6ebbbc0d4164
Thore Engel <thore.engel@idiv.de>
2025-02-18 16:19:24 +0100

    Update code for paper submission fix

--- Figure2.R
+++ Figure2.R
@@ -6,21 +6,34 @@ library(rgbif)
 library(geodata)
 library(sf)
 library(units)
+library(lubridate)
 
 theme_set(theme_cowplot()) # ggplot theme
 options(scipen=999) # Avoiding scientific notation of numbers
 
-# Retrieve data gbif (https://www.gbif.org/occurrence/download/0017597-241024112534372)
-dat<-occ_download_get("0017597-241024112534372",overwrite = F) |> occ_download_import()
+# Retrieve data gbif 
+dat<-occ_download_get("0002787-250214102907787",overwrite = F) |> occ_download_import() # 17th March 2025
 
-#remove columns with only NAs. They were added by GBIF.
+
+# Check data --------------------------------------------------------------
+
+# remove columns with only NAs. They were added by GBIF.
+
 dat <-
   dat |>
   select(-where(function(x) all(is.na(x))))
 
+# Filter by year. For the paper we use data as of 31st of December 2025
+dat<- dat |> filter(year<2025)
 
-# Check data --------------------------------------------------------------
-summary(dat)
+# check dates
+dat<- dat |>
+  mutate(eventDate= ymd(eventDate)) |> 
+  mutate(dateIdentified= ymd_hms(dateIdentified))
+# this has been fixed in the dynamic data
+
+
+dat |> filter(dateIdentified<eventDate) |> View()
 
 # Data without taxonomic match by gbif
 unmatched_taxa <-
@@ -61,7 +74,7 @@ sum(unmatched_taxa$n) - sum(unmatched_taxa2$n)
 # save unmatched taxa
 write_excel_csv(unmatched_taxa2, "unmatched_taxa.csv")
 
-# 4330 more records matching
+# 4398 more records matching
 
 # Plot taxonomic distribution ---------------------------------------------
 
@@ -76,17 +89,17 @@ Plot1 <-
       class == "Amphibia" ~ "Amphibians",
       kingdom == "Plantae" ~ "Plants",
       kingdom == "Fungi" ~ "Fungi",
-      class == "Testudines" | class == "Squamata" ~ "Reptiles",
-      .default = "other"
+      class == "Testudines" | class == "Squamata" ~ "'Reptiles'",
+      .default = "Other"
     )
   ) |>
   group_by(taxon) |>
   summarise(number = n()) |>
   ggplot() +
-  geom_bar(aes(x = reorder(taxon,-number), y = number),
+  geom_bar(aes(x = reorder(taxon,-number), y = number/1000),
            stat = "identity",
            fill = "#4daf4a") +
-  labs(x = "Taxonomic group", y = "# Records")
+  labs(x = "Taxonomic group", y = "# Records (x 1000)")
 
 
 # Spatial distribution -----------------------------------------------
@@ -98,7 +111,7 @@ records_per_country<-
   summarise(number= n()) |> 
   arrange(desc(number))
 
-# 5423 records in France
+# 5671 records in France
 # few records in other countries, mostly neighboring Germany
 
 records_per_country_percent<-
@@ -168,21 +181,24 @@ dat |>
   group_by(year) |> 
   summarise(number=n()) |> 
   ggplot()+
-  geom_bar(aes(x=year, y=number), stat= "identity", fill = "#4daf4a")+
-  labs(y="# Records", x="Year")
+  geom_bar(aes(x=year, y=number/1000), stat= "identity", fill = "#4daf4a")+
+  labs(y="# Records (x 1000)", x="Year")+
+  scale_x_continuous(breaks = c(2010,2015,2020,2024))
 
 # What percentage of data are before 2010 
 before2010 <- dat |> filter(year<2010) |> nrow()
-1-(before2010*100/nrow(dat)) # 0.09534075%
+1-(before2010*100/nrow(dat)) # 0.1115762%
 
 # Make combined figure -------------------------------------------------------------
 
-# right column
-col1<-plot_grid(Plot1, plot3, nrow = 2, align = "hv", labels = c("B","C"))
+### export taxonomic plot for collage
 
-# everything
-fig2<-plot_grid(plot2, col1, nrow = 1, labels = c("A",NULL), rel_widths = c(1,1.5))
+Plot1a <- Plot1 +
+  theme(
+    axis.text = element_text(size = 30), # Adjust axis text size
+    axis.title = element_text(size = 32) # Optionally, adjust axis title size
+  )
+save_plot("plot1.jpg", Plot1a, base_asp = 18/5.76,base_height = 5.76)
 
-# save
-save_plot("figure2.jpg", fig2,ncol = 2, nrow = 2 )
-
+fig2<-plot_grid(plot2, plot3, nrow = 1, labels = c("A","B"), rel_widths = c(1,1.5))
+save_plot("figure2.jpg", fig2,ncol = 2, nrow = 1,base_asp = 1)
